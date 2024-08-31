ee7b7bc7e09ed300b2324da8cfb9631be39686b4
Saad Misk <221191@ppu.edu.ps>
2024-08-31 11:25:04 +0300

    initial commit: set up project structure

---
+++ Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem
+{
+    public class Product
+    {
+        private string Name {  get; set; }
+        private string? Description { get; set; }
+        private double Price {  get; set; }
+        private int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder st = new StringBuilder();
+            st.Append($"Name:{Name}, Price: ${Price}, Quantity:{Quantity}");
+            if (Description == null)
+            {
+                st.Append("\nDescription: no inforamtion!");
+            }
+            else
+            {
+                st.Append($"\nDescription: {Description}");
+            }
+
+            return st.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // Initialize the Product instance within the Main method
+            Product p = new Product("bags", 60, 100);
+
+            // Output the product details
+            Console.WriteLine("hi");
+            Console.WriteLine(p.ToString());
+        }
+
+    }
+}
